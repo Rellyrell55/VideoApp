e43e413b8e7a8cd5eec9d4c8da90f35ab2a2d31d
Terrell Cooper <TerrellCoop@gmail.com>
2021-04-27 13:36:34 -0600

    created basic navbar still need to work with the reponsiveness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello
-      </h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <h1>
+          Hello
+        </h1>
+      </div>
+    </Router>
   );
 }
 
